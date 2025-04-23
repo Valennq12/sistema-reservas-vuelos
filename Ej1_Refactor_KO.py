316d0fe4e98ef3f08ab9966890a1e3ba41bfe673
Valennq12 <valenlovecrafts@gmail.com>
2025-04-23 18:30:07 +0200

    Refactor: extraída función capturar_datos_pasajero para evitar duplicación de código en main()

--- Ej1_Refactor_KO.py
+++ Ej1_Refactor_KO.py
@@ -46,6 +46,14 @@ def reservar_vuelo(lista, numero_vuelo, pasajero, cantidad):
                 return
     print("No se encontró ningún vuelo con el número especificado.")
 
+def capturar_datos_pasajero():
+            nombre = input("Ingrese su nombre: ")
+            apellido = input("Ingrese su apellido: ")
+            edad = int(input("Ingrese su edad: "))
+            telefono = input("Ingrese su número de teléfono: ")
+            correo = input("Ingrese su correo electrónico: ")
+            return Pasajero(nombre, apellido, edad, telefono, correo)
+
 
 def main():
     vuelos = [
@@ -60,14 +68,7 @@ def main():
     if opcion == '1':
         mostrar_vuelos_disponibles(vuelos)
     elif opcion == '2':
-        #extraer método
-        n = input("Ingrese su nombre: ")
-        a = input("Ingrese su apellido: ")
-        e = int(input("Ingrese su edad: "))
-        t = input("Ingrese su número de teléfono: ")
-        c = input("Ingrese su correo electrónico: ")
-
-        pasajero = Pasajero(n, a, e, t, c)
+        pasajero = capturar_datos_pasajero()
 
         #extraer método
         numero = input("Ingrese el número de vuelo que desea reservar: ")
